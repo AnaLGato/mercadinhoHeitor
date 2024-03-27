516df38fb2ff26a2bbc224636d293287fc52ca4a
AnaLGato <126986673+AnaLGato@users.noreply.github.com>
2024-03-27 18:14:22 -0300

    Add files via upload

---
+++ mercadinho/mercadinho/Produto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mercadinho
+{
+    internal class Produto
+    {
+        public string Codigo;
+        public string Descricao;
+        public float Preco;
+        public float Qntestoque;
+        public string Coisa;
+
+        public Produto(string codigo, string descricao, float preco, float qntestoque, string coisa)
+        {
+            Codigo = codigo;
+            Descricao = descricao;
+            Preco = preco;
+            Qntestoque = qntestoque;
+            Coisa = coisa;
+        }
+    }
+}
---
+++ mercadinho/mercadinho/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace mercadinho
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void linha()
+            {
+                for (int i = 0; i == 100; i++)
+                {
+                       Console.Write("-");
+
+                }
+
+                Console.WriteLine();
+            
+            }
+            
+            static void Imprimir(string codigo, string descricao, float preco, float qntestoque, string coisa)
+            {
+                Console.WriteLine("| {0,-13} | {1} | {2} | R$ {3}  {4} |",codigo, descricao, preco, qntestoque, coisa);
+            }
+             
+            
+            
+
+            Produto[] carrinho = new Produto[12];
+
+            carrinho[0] = new Produto("7891025101604", "Leite", 3.0f, 15.0f, "l");
+            carrinho[1] = new Produto("7891025101604", "Leite", 3.0f, 15.0f, "l");
+            carrinho[2] = new Produto("7891025101604", "Leite", 3.0f, 15.0f, "l");
+            carrinho[3] = new Produto("7891025101604", "Leite", 3.0f, 15.0f, "l");
+            carrinho[4] = new Produto("7891025101604", "Leite", 3.0f, 15.0f, "l");
+            carrinho[5] = new Produto("7891025101604", "Leite", 3.0f, 15.0f, "l");
+            carrinho[6] = new Produto("7891025101604", "Leite", 3.0f, 15.0f, "l");
+            carrinho[7] = new Produto("7891025101604", "Leite", 3.0f, 15.0f, "l");
+            carrinho[8] = new Produto("7891025101604", "Leite", 3.0f, 15.0f, "l");
+            carrinho[9] = new Produto("7891025101604", "Leite", 3.0f, 15.0f, "l");
+            carrinho[10] = new Produto("7891025101604", "Leite", 3.0f, 15.0f, "l");
+            carrinho[11] = new Produto("7891025101604", "Leite", 3.0f, 15.0f, "l");
+            linha();
+            foreach(var produto in carrinho)
+            {
+                Imprimir(produto.Codigo, produto.Descricao, produto.Preco, produto.Qntestoque, produto.Coisa);
+            }
+
+            linha();
+
+        }
+    }
+}
